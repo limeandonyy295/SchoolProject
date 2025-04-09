be279fa48b313f31995008966149653681e7e38d
limeandonyy295 <limeandonyy295@gmail.com>
2025-04-09 07:30:31 +0000

    Add 28.cpp

---
+++ src/28.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "This is a simple school project written in C++."; // This line prints the message to the console.
+    return 0; // The program exits after printing the message.
+}
