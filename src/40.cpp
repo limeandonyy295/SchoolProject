b706e880eaf6931204fd9d84e013a3d297a0822e
limeandonyy295 <limeandonyy295@gmail.com>
2025-05-25 00:34:30 +0000

    Add 40.cpp

---
+++ src/40.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int height = 3;
+    int width = 2;
+    
+    if (height > width) {
+        cout << "The height is greater than the width." << endl;
+    } else {
+        cout << "The height is not greater than the width." << endl;
+    }
+    
+    return 0;
+}
